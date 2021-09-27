a487503158f1cfd5fcfdd30670c834fd0922b319
Adam Weiss <aweiss@acvauctions.com>
2021-09-27 17:04:10 -0400

    Handle null context and cancelled requests better

--- src/DynamicJsonPropertyNamingPolicy.Tests/JsonNamingPolicies/DynamicSystemTextJsonInputFormatterUnitTests.cs
+++ src/DynamicJsonPropertyNamingPolicy.Tests/JsonNamingPolicies/DynamicSystemTextJsonInputFormatterUnitTests.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using DynamicJsonPropertyNamingPolicy.JsonNamingPolicies;
 using Xunit;
+using System;
+using System.Threading;
 
 namespace DynamicJsonPropertyNamingPolicy.Tests.JsonNamingPolicies
 {
@@ -57,6 +59,69 @@ namespace DynamicJsonPropertyNamingPolicy.Tests.JsonNamingPolicies
             Assert.NotNull(actual.Model);
             Assert.IsType<SampleType>(actual.Model);
             Assert.Equal(policyName, ((SampleType)actual.Model).NamingPolicy);
+        }
+
+        [Fact]
+        public async void NullContextThrowsException()
+        {
+            // Arrange
+            Encoding encoding = Encoding.UTF8;
+            DynamicSystemTextJsonInputFormatter formatter = new();
+
+            // Act
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await formatter.ReadRequestBodyAsync(null, encoding));
+
+            // Assert
+        }
+
+        [Fact]
+        public async void NullEncodingThrowsException()
+        {
+            // Arrange
+            InputFormatterContext context = new(
+                new DefaultHttpContext(),
+                nameof(SampleType),
+                new ModelStateDictionary(),
+                new EmptyModelMetadataProvider().GetMetadataForType(typeof(SampleType)),
+                (stream, encoding) => new StreamReader(stream, encoding));
+            DynamicSystemTextJsonInputFormatter formatter = new();
+
+            // Act
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await formatter.ReadRequestBodyAsync(context, null));
+
+            // Assert
+        }
+
+        [Fact]
+        public async void RequestAbortedReturnsNoValue()
+        {
+            // Arrange
+            InputFormatterResult expected = InputFormatterResult.NoValue();
+            Encoding encoding = Encoding.UTF8;
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.TryAddSingleton<ILogger<SystemTextJsonInputFormatter>>(
+                new NullLogger<SystemTextJsonInputFormatter>());
+            ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(new ServiceProviderOptions
+            {
+                ValidateOnBuild = true,
+                ValidateScopes = true
+            });
+            DefaultHttpContext httpContext = new();
+            httpContext.RequestServices = serviceProvider;
+            InputFormatterContext context = new(
+                httpContext,
+                nameof(SampleType),
+                new ModelStateDictionary(),
+                new EmptyModelMetadataProvider().GetMetadataForType(typeof(SampleType)),
+                (stream, encoding) => new StreamReader(stream, encoding));
+            DynamicSystemTextJsonInputFormatter formatter = new();
+            httpContext.RequestAborted = new CancellationToken(true);
+
+            // Act
+            InputFormatterResult actual = await formatter.ReadRequestBodyAsync(context, encoding);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
